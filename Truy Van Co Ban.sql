9d528dfcd87cc9905ff60c843b2939674ad83f29
Tengu146 <160589774+Tengu146@users.noreply.github.com>
2025-03-16 22:56:38 +0700

    các kiểu truy vấn từ cơ bản đến nâng cao

---
+++ Truy Van Co Ban.sql
@@ -0,0 +1,25 @@
+﻿--SELECT - Đếm số thành viên theo tỉnh
+SELECT t.TenTinh, COUNT(dv.MaDoanVien) AS SoLuongDoanVien  
+FROM Tinh t  
+INNER JOIN DoanVien dv ON t.MaTinh = dv.MaTinh  
+GROUP BY t.TenTinh;  
+
+-- INSERT
+INSERT INTO Tinh (TenTinh) VALUES ('Bình Dương');
+INSERT INTO DoanVien (HoTen, NgaySinh, QueQuan, MaTinh, MaDanToc, MaTonGiao) VALUES ('Nguyễn Văn Z', '2005-01-01', 'Thủ Dầu Một', (SELECT MaTinh FROM Tinh WHERE TenTinh = 'Bình Dương'), 1, 1);
+INSERT INTO ChuongTrinh (TenCT, ThoiGian, DiaDiem) VALUES ('Hội thảo kỹ năng mới', '2025-12-31 10:00:00', 'Bình Dương');
+INSERT INTO ToChucCT (MaDoanVien, MaCT) VALUES ((SELECT MaDoanVien FROM DoanVien WHERE HoTen = 'Nguyễn Văn Z'), (SELECT MaCT FROM ChuongTrinh WHERE TenCT = 'Hội thảo kỹ năng mới'));
+INSERT INTO ThamGiaCT (MaDoanVien, MaCT, DanhGia) VALUES (1, (SELECT MaCT FROM ChuongTrinh WHERE TenCT = 'Hội thảo kỹ năng mới'), 'Rất tốt');
+
+-- UPDATE
+UPDATE Tinh SET TenTinh = 'Thành phố Hồ Chí Minh' WHERE TenTinh = 'Hồ Chí Minh';
+UPDATE DoanVien SET NgaySinh = '1999-01-01' WHERE HoTen = 'Nguyễn Văn An';
+UPDATE ThamGiaCT SET DanhGia = 'Không tốt' WHERE MaDoanVien = 11 AND MaCT = (SELECT MaCT FROM ChuongTrinh WHERE TenCT = 'Hội thảo kỹ năng mới');
+
+-- DELETE (cần kiểm tra ràng buộc trước khi xóa)
+-- Kiểm tra trước khi xóa tỉnh
+SELECT * FROM DoanVien WHERE MaTinh = (SELECT MaTinh FROM Tinh WHERE TenTinh = 'Hải Phòng');
+-- Xóa đoàn viên trước nếu cần thiết, hoặc sử dụng ON DELETE CASCADE
+DELETE FROM DoanVien WHERE MaDoanVien = 16;
+DELETE FROM ChuongTrinh WHERE MaCT = 1;
+DELETE FROM Tinh WHERE MaTinh = 1;  
---
+++ Truy vấn nâng cao.sql
@@ -0,0 +1,29 @@
+﻿--INNER JOIN - Danh sách đoàn viên tham gia chương trình
+SELECT dv.HoTen, ct.TenCT, ct.ThoiGian, ct.TrangThai
+FROM DoanVien dv
+INNER JOIN ThamGiaCT tg ON dv.MaDoanVien = tg.MaDoanVien
+INNER JOIN ChuongTrinh ct ON tg.MaCT = ct.MaCT;
+
+--GROUP BY - Đếm số đoàn viên theo tỉnh
+SELECT t.TenTinh, COUNT(dv.MaDoanVien) AS SoLuongDoanVien
+FROM Tinh t
+INNER JOIN DoanVien dv ON t.MaTinh = dv.MaTinh
+GROUP BY t.TenTinh;
+
+--HAVING - Lọc tỉnh có trên 5 đoàn viên
+SELECT t.TenTinh, COUNT(dv.MaDoanVien) AS SoLuongDoanVien
+FROM Tinh t
+INNER JOIN DoanVien dv ON t.MaTinh = dv.MaTinh
+GROUP BY t.TenTinh
+HAVING COUNT(dv.MaDoanVien) > 5;
+
+--SUBQUERY - Lấy đoàn viên tham gia chương trình gần nhất
+SELECT dv.HoTen, dv.NgaySinh, ct.TenCT
+FROM DoanVien dv
+INNER JOIN ThamGiaCT tg ON dv.MaDoanVien = tg.MaDoanVien
+INNER JOIN ChuongTrinh ct ON tg.MaCT = ct.MaCT
+WHERE ct.ThoiGian = (
+    SELECT MAX(ThoiGian)
+    FROM ChuongTrinh
+);
+
